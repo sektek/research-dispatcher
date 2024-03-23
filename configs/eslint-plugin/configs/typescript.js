365b9900537b343f443f18b3ed4fec72ce5a0d7e
Edward Kelly <eddie@sektek.net>
2024-03-23 12:15:26 +0000

    fix: code cleanup

--- configs/eslint-plugin/configs/typescript.js
+++ configs/eslint-plugin/configs/typescript.js
@@ -13,7 +13,7 @@ module.exports = {
     },
   },
   rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error"
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
 };
