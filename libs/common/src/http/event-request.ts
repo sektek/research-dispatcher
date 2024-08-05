3a7087fee9ae35ef34d4b7f67166d5625ce1ca40
Edward Kelly <eddie@sektek.net>
2024-08-05 14:42:38 +0000

    fix: added default type for EventRequest

--- libs/common/src/http/event-request.ts
+++ libs/common/src/http/event-request.ts
@@ -1,6 +1,6 @@
 import { Event } from '../event.js';
 import { Request } from './request.js';
 
-export interface EventRequest<T extends Event> extends Request {
+export interface EventRequest<T extends Event = Event> extends Request {
   body: T;
 }
